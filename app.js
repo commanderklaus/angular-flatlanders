3245ed26cc07e255d03698cc112377ff5daa6176
Klaus Komenda <me@klauskomenda.com>
2016-03-12 13:37:53 +0100

    refactoring

--- app.js
+++ app.js
@@ -1,42 +1,14 @@
 (function () {
 	var app = angular.module('store', ['store-products']);
 
-	var gems = [
-		{
-			price: 2,
-			name: 'Dodecahedron',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis quis quam vel tincidunt. Vestibulum ullamcorper tincidunt est.',
-			canPurchase: true,
-			soldOut: false,
-			images: [{
-				full: "http://curvebank.calstatela.edu/hamiltoncircuit/dodecasolid/dodecaforever.gif",
-				thumb: "http://curvebank.calstatela.edu/hamiltoncircuit/dodecasolid/dodecaforever.gif"
-			}],
-			reviews: [
-			{
-				stars: 5,
-				body: "I love this product",
-				author: "joe@joeschmo.com" 
-			},
-			{
-				stars: 1,
-				body: "I love this product",
-				author: "joe@joeschmo.com" 
-			}
-			]
-		},
-		{
-			price: 2.5,
-			name: 'Pentagonal Gem',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis quis quam vel tincidunt. Vestibulum ullamcorper tincidunt est.',
-			canPurchase: false,
-			soldOut: false
-		}
-	];
+	app.controller('StoreController', ['$http', '$log', function ($http, $log) { // dependency injecton
+		var store = this;
+		store.products = [];
 
-	app.controller('StoreController', function () {
-		this.products = gems;
-	});
+		$http({method: 'GET', url: 'products.json'}).success(function (data) {
+			store.products = data;
+		});
+	}]);
 
 	app.controller('ReviewController', function () {
 		this.review = {};
